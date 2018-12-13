1ef611ca7de40473b0de5abab33dff572f65d1a6
Oleksii Karban <kenifunogun@gmail.com>
2018-12-13 16:45:39 +0200

    mobile menu

--- js/script.js
+++ js/script.js
@@ -1,74 +1,131 @@
-let feed = new Instafeed({
-  get: "user",
-  userId: 3413063057,
-  clientId: "668116fe96b34ada9024flc0e6917bba",
-  accessToken: "3413063057.668116f.f510426802f2400a8578c8110dd35a93",
-  limit: 6,
-  target: "instafeed",
-  sortBy: "most-recent",
-  resolution: "standard_resolution",
-  filter: function(image) {
-    let MAX_LENGTH = 47;
+function instafeed() {
+  let feed = new Instafeed({
+    get: "user",
+    userId: 3413063057,
+    clientId: "668116fe96b34ada9024flc0e6917bba",
+    accessToken: "3413063057.668116f.f510426802f2400a8578c8110dd35a93",
+    limit: 6,
+    target: "instafeed",
+    sortBy: "most-recent",
+    resolution: "standard_resolution",
+    filter: function(image) {
+      let MAX_LENGTH = 47;
+  
+      if (image.caption && image.caption.text) {
+        image.short_caption = image.caption.text.slice(0, MAX_LENGTH);
+        image.short_caption += "...";
+      } else {
+        image.short_caption = "";
+      }
+  
+      return true;
+    },
+    template:   '<div class="col-xs-12 col-sm-6 col-md-4 p-3">' +
+                '<a href="{{link}}" target="_blank">' +
+                '<div class="img-featured-container  border border-dark rounded">' +
+                '<div class="img-backdrop"></div>' +
+                '<div class="description-container">' +
+                '<p class="caption">{{model.short_caption}}</p>' +
+                '<span class="likes"><i class="fa fa-heart"></i> {{likes}}</span>' +
+                '<span class="comments"><i class="fa fa-comment"></i> {{comments}}</span>' +
+                '</div>' +
+                '<img src="{{image}}" class="img-responsive">' +
+                '</div>' +
+                '</a>' +
+                '</div>'
+  });
 
-    if (image.caption && image.caption.text) {
-      image.short_caption = image.caption.text.slice(0, MAX_LENGTH);
-      image.short_caption += "...";
+  return feed.run();
+}
+
+function menuExpand( $body, $grayBack, $menuMobile ) {
+  if ( $body.length && $grayBack.length && $menuMobile ) {
+
+    if ( !$grayBack.queue( "fx" ).length && !$menuMobile.queue( "fx" ).length ) {
+      $grayBack.fadeIn( {
+        duration: 200,
+        start:function() {
+          $menuMobile.animate( {
+            "left": "+=100%"
+          }, 400 );
+          $body.css( { overflow: "hidden" } );
+        }
+      });
     } else {
-      image.short_caption = "";
+      //console.log( "The expand animation is not over!" );
+      return false;
     }
-
-    return true;
-  },
-  template:   '<div class="col-xs-12 col-sm-6 col-md-4 p-0 pr-3 pb-3">' +
-              '<a href="{{link}}" target="_blank">' +
-              '<div class="img-featured-container  border border-dark rounded">' +
-              '<div class="img-backdrop"></div>' +
-              '<div class="description-container">' +
-              '<p class="caption">{{model.short_caption}}</p>' +
-              '<span class="likes"><i class="fa fa-heart"></i> {{likes}}</span>' +
-              '<span class="comments"><i class="fa fa-comment"></i> {{comments}}</span>' +
-              '</div>' +
-              '<img src="{{image}}" class="img-responsive">' +
-              '</div>' +
-              '</a>' +
-              '</div>'
-});
-
-feed.run();
-
-$(document).ready(function() {
-  $('#content').show();
-  $('#msg').hide();
-});
-
-let isMobileOpen = null;
-
-function menuMobileOpen() {
-  //let headerHeight = document.getElementById("header").offsetHeight;
-  let menuMobile = document.getElementById("menu-mobile");
-
-  if (menuMobile.style.display === "block") {
-    isMobileOpen = true;
+    
   } else {
-    isMobileOpen = false;
+    //console.log( "Not all elements for menuExpand() found!" );
+    return false;
   }
 
-  if (isMobileOpen === false) {
-    //menuMobile.style.top = headerHeight + 'px';
+  return true;
+}
 
-    document.getElementById("menu-mobile").style.display = "block";
-    document.getElementById("gray_back").style.display = "block";
+function menuCollapse( $body, $grayBack, $menuMobile ) {
+  if ( $body.length && $grayBack.length && $menuMobile ) {
 
-    isMobileOpen = true;
+    if ( !$grayBack.queue( "fx" ).length && !$menuMobile.queue( "fx" ).length ) {
+      $grayBack.fadeOut( {
+        duration: 250,
+        start: function() {
+          $menuMobile.animate( {
+            "left": "-=100%"
+          }, 250 );
+        },
+        complete:function() {
+          $body.css( { overflow: "auto" } );
+        }
+      });
+    } else {
+      //console.log( "The collapse animation is not over!" );
+      return false;
+    }
+    
   } else {
-    menuMobileCollapse();
-    isMobileOpen = false;
+    //console.log( "Not all elements for menuCollapse() found!" );
+    return false;
   }
+
+  return true;
 }
 
-function menuMobileCollapse() {
+$( document ).ready( function() {
+  let $body = $( "body" );
+  let $grayBack = $( "#gray_back" );
+  let $menuMobile = $( "#menu-mobile" );
 
-  document.getElementById("menu-mobile").style.display = "none";
-  document.getElementById("gray_back").style.display = "none";
+  instafeed();
 
-}
+  $( "#hamburger-menu-btn" ).on( "click", function() {
+    menuExpand( $body, $grayBack, $menuMobile );
+  });
+
+  $( "#gray_back, #menu-mobile a" ).on( "click", function() {
+    menuCollapse( $body, $grayBack, $menuMobile );
+  });
+
+  $( document ).on( "scroll", function() {
+    if ( $( this ).scrollTop() > $( "#header" ).height() ) {
+      $( "#header" ).animate( {
+        opacity: "0.95"
+      }, 10 );
+    } else {
+      $( "#header" ).animate( {
+        opacity: "1"
+      }, 10 );
+    }
+  });
+  
+});
+
+$( window ).on( "load", function() {
+  $( "body" ).animate({
+    height: [ "toggle", "swing" ],
+    opacity: "toggle"
+  }, 1000, "linear", function() {
+    
+  });
+});
